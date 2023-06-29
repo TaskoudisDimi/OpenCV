fa7db53810596fee44911b81434641009261f165
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-06-29 15:40:26 +0300

    Scale Resize

--- read.py
+++ read.py
@@ -1,9 +1,22 @@
 import cv2 as cv
 
 
-img = cv.imread('C:/Users/chris/Desktop/Dimitris/Tutorials/OpenCV/Resources/Photos/cat.jpg')
-cv.imshow('Cat',img)
+# img = cv.imread('C:/Users/chris/Desktop/Dimitris/Tutorials/OpenCV/OpenCV/Resources/Photos/cat.jpg')
+# cv.imshow('Cat',img)
+# cv.waitKey(0)
 
-cv.waitKey(0)
+#Reading Videos
+capture = cv.VideoCapture('C:/Users/chris/Desktop/Dimitris/Tutorials/OpenCV/OpenCV/Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
 
 
