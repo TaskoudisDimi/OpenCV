aca08f21bf68a1e82310fcc8f70004e7acdb3030
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-07-06 12:43:30 +0300

    bitwise, smoothing, masking

--- smoothing.py
+++ smoothing.py
@@ -5,6 +5,21 @@ img = cv.imread('C:/Users/chris/Desktop/Dimitris/Tutorials/OpenCV/OpenCV/Resourc
 
 cv.imshow('Cat', img)
 
+# Average
+average = cv.blur(img, (7,7))
+cv.imshow('Average Blur', average)
+
+
+gauss = cv.GaussianBlur(img, (7,7), 0)
+cv.imshow('Gaussian Blur', gauss)
+
+median = cv.medianBlur(img, 7)
+cv.imshow('Median Blur', median)
+
+bilateral = cv.bilateralFilter(img, 10, 15, 15)
+cv.imshow('Bilateral', bilateral)
+
 
 
 cv.waitKey(0)
+
