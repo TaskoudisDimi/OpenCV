f80cec881f7a2234e3077b71db2ba44ca5bc2539
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-10-02 00:15:05 +0300

    start VirtualDragAndDrop

--- VirtualDragAndDrop/main.py
+++ VirtualDragAndDrop/main.py
@@ -2,31 +2,23 @@
 import cv2 as cv
 import time
 import mediapipe as mp
+from cvzone.HandTrackingModule import HandDetector
 
 
+cap = cv.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = HandDetector(detectionCon=0.8)
 
-# pTime = 0
-# ctime = 0
-    
-# video = cv.VideoCapture('C:/Users/chris/Desktop/Dimitris/Tutorials/OpenCV/OpenCV/ComputerVision/Videos/Video1.mp4')
-# detector = handModule.handDetector()
-# while True:
-#         #Example Video
-        
-#         success, img = video.read()
-        
-#         # img = img[500:1500, 500:1000, :]
-#         img = detector.findHands(img)
-#         lmList = detector.findPosition(img)
-#         if(len(lmList) != 0):
-#             print(lmList[4])
 
-#         cTime = time.time()
-#         fps = 1/(cTime-pTime)
-#         pTime = cTime
-#         cv.putText(img, str(int(fps)), (10,10), cv.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+while True:
+    success, img = cap.read()
+    img = cv.cvtColor( img, cv.COLOR_BGR2RGB)
+    img = detector.findHands(img)
+    # lmlist, _ = detector.findPosition(img)
+    cv.imshow("Image", img)
+    cv.waitKey(1)
 
-#         cv.imshow('Video', img)
-#         cv.waitKey(1)
+
 
 
