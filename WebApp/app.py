22414c84d10f38721a0e9673252fa9d5b13079b4
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-10-30 12:27:37 +0200

    Fixes about html,css

--- WebApp/app.py
+++ WebApp/app.py
@@ -5,22 +5,42 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+app.static_folder = 'static'
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('Home.html')
 
-@app.route('/FaceRecognition', methods=['GET'])
-def FaceRecognition():
-    return render_template('FaceRecognition.html')
+
 
 @app.route('/EditImage', methods=['GET'])
 def EditImage():
     return render_template('EditImage.html')
 
+@app.route('/EditVideo', methods=['GET'])
+def EditVideo():
+    return render_template('EditVideo.html')
 
-@app.route('/HandTracking', methods=['GET'])
-def HandTracking():
-    return render_template('HandTracking.html')
+@app.route('/FingerCounting', methods=['GET'])
+def FingerCounting():
+    return render_template('FingerCounting.html')
+
+@app.route('/FaceRecognition', methods=['GET'])
+def FaceRecognition():
+    return render_template('FaceRecognition.html')
+
+@app.route('/MouseControl', methods=['GET'])
+def MouseControl():
+    return render_template('MouseControl.html')
+
+@app.route('/GymTracker', methods=['GET'])
+def GymTracker():
+    return render_template('GymTracker.html')
+
+@app.route('/Scanner', methods=['GET'])
+def Scanner():
+    return render_template('Scanner.html')
+
 
 
 @app.route("/process_video", methods=["POST"])
